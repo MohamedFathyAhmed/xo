4195e2d0a8a27811900756ee6703a3f18a6b1b79
mohamed-ie <mohammedie98@gmail.com>
2022-12-04 17:15:13 +0200

    feat:board-ui

---
+++ src/xo/board/FXMLDocumentController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xo.board;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author mohamed
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    
+
+    @FXML
+    private void recordButtonClicked() {
+        
+    }
+
+    @FXML
+    private void leaveButtonClicked() {
+       
+    }
+    
+    @FXML
+    private void boardButtonEntered(ActionEvent event){
+        
+    }
+    
+    @FXML
+    private void boardButtonExit(ActionEvent event){
+        
+    }
+
+    @FXML
+    private void boardButtonClicked(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    }
+
+}
