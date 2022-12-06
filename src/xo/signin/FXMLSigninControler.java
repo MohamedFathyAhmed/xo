e7d347623f905ae6195be4b0ee2b7f7c3722833d
Marina <marinaatef412@gmail.com>
2022-12-06 19:09:03 +0200

    feat: signin

---
+++ src/xo/signin/FXMLSigninControler.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xo.signin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Marina
+ */
+public class FXMLSigninControler implements Initializable {
+
+    @FXML
+    private ImageView levelsImagetwo;
+    @FXML
+    private Button btnBack;
+    @FXML
+    private TextField userNameTextField;
+    @FXML
+    private TextField passwordTextField;
+    @FXML
+    private Label userNameLabel;
+    @FXML
+    private Label passwordLabel;
+    @FXML
+    private Label signInLabel;
+    @FXML
+    private Button bntLogin;
+    @FXML
+    private Button btnSignup;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
