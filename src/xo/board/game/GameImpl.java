fbecf3242d46ce0454734faa90ff3ee084f585a4
Myfile <eng.mohameed.fathy@gmail.com>
2022-12-09 00:49:20 +0200

    feat :  easyMode game

--- src/xo/board/game/GameImpl.java
+++ src/xo/board/game/GameImpl.java
@@ -4,6 +4,9 @@
  */
 package xo.board.game;
 
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  *
  * @author mo_fathy
@@ -32,4 +35,34 @@ public class GameImpl implements Game{
     }
     
     
+public static void easyMode(String board) {
+        int emtyLocation[];
+        emtyLocation = new int[0];
+        int arrNum = 0;
+        for (int i = 0; i < 9; i++) {
+
+            if (board.charAt(i) == '-') {
+               emtyLocation = append(emtyLocation, i);
+                arrNum++;
+            }
+        }
+        int randNum = getRandomElement(emtyLocation);
+        System.out.println(randNum);
+
+    }
+
+    public static int getRandomElement(int[] arr) {
+        return arr[ThreadLocalRandom.current().nextInt(arr.length)];
+    }
+    
+      static <T> int[] append(int[] arr, int element) {
+        final int N = arr.length;
+        arr = Arrays.copyOf(arr, N + 1);
+        arr[N] = element;
+        return arr;
+    }
+
 }
+
+
+ 
