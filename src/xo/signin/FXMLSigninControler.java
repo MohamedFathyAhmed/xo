5b666d015ff6f852908501c18df23c37b4cfca3a
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2022-12-18 20:41:05 +0200

    add : dialog

--- src/xo/signin/FXMLSigninControler.java
+++ src/xo/signin/FXMLSigninControler.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,10 +19,17 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
 
 /**
  * FXML Controller class
@@ -137,6 +145,46 @@ Thread thread;
 
     @FXML
     private void ipButtonClicked(ActionEvent event) {
+          // Create the custom dialog.
+    Dialog<Pair<String, String>> dialog = new Dialog<>();dialog.setTitle("Connect to server");dialog.setHeaderText("Please insert remoteIP and portNumber");
+
+    ButtonType connectButtonType = new ButtonType("Connect",
+            ButtonBar.ButtonData.OK_DONE);dialog.getDialogPane().getButtonTypes().addAll(connectButtonType,ButtonType.CANCEL);
+
+    GridPane grid = new GridPane();grid.setHgap(10);grid.setVgap(10);grid.setPadding(new Insets(20,150,10,10));
+
+    TextField ipAddress = new TextField();ipAddress.setPromptText("Please enter IP:");
+    TextField portNumber = new TextField();portNumber.setPromptText("Please enter portNumber:");
+
+    grid.add(new Label("Ip Address:"),0,0);grid.add(ipAddress,1,0);grid.add(new Label("Port Number:"),0,1);grid.add(portNumber,1,1);
+
+    Node connectButton = dialog.getDialogPane().lookupButton(connectButtonType);connectButton.setDisable(true);
+
+    ipAddress.textProperty().addListener((observable,oldValue,newValue)->
+    {
+        connectButton.setDisable(newValue.trim().isEmpty());
+    });
+
+    dialog.getDialogPane().setContent(grid);
+
+    Platform.runLater(()->ipAddress.requestFocus());
+
+    dialog.setResultConverter(dialogButton->
+    {
+        if (dialogButton == connectButtonType) {
+            return new Pair<>(ipAddress.getText(), portNumber.getText());
+        }
+        return null;
+    });
+
+    Optional<Pair<String, String>> result = dialog.showAndWait();
+
+    result.ifPresent(connectToServer->
+    {
+        String remoteIP = connectToServer.getKey();
+        String portNum = connectToServer.getValue();
+        // set ip and port
+});
     }
     
 }
