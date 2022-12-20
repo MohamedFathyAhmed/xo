c29d5d15b71c01d3e5460a8f0b2ba117a9d5b859
Hudakamal6 <hudakamal9215@gmail.com>
2022-12-20 19:40:36 +0200

    fix : navugation

--- src/xo/utlis/TicTacToeNavigator.java
+++ src/xo/utlis/TicTacToeNavigator.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.util.Stack;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 
 import javafx.fxml.FXMLLoader;
@@ -16,6 +18,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import xo.online.signin.FXMLSigninControler;
 //import xo.board.BoardMultiPlayerOflineModeController;
 //import xo.board.BoardMultiPlayerOnlineModeController;
 //import xo.board.BoardSinglePlayerModeController;
@@ -35,6 +38,10 @@ public class TicTacToeNavigator {
     public static final NavigationDestination MODES = new NavigationDestination("modes/FXMLModes.fxml", "MODES");
     public static final NavigationDestination ONE_PLAYER_NAME_CHOOSER = new NavigationDestination("one_player_name_chooser/FXMLOnePlayerNameChooser.fxml", "PLAYER NAME");
     public static final NavigationDestination TWO_PLAYER_NAME_CHOOSER = new NavigationDestination("two_players_name_chooser/FXMLTwoPlayersNameChooser.fxml", "PLAYER NAME");
+    public static final NavigationDestination SIGNIN = new NavigationDestination("online/signin/FXMLSignin.fxml", "SIGNIN");
+    public static final NavigationDestination SIGNUP = new NavigationDestination("online/signup/FXMLSignup.fxml", "SIGNUP");
+    public static final NavigationDestination ONLINE_PLAYERS = new NavigationDestination("online/online_players/FXMLOnlinePlayers.fxml", "ONLINLE PLAYERS");
+    public static final NavigationDestination ONLINE_BOARD = new NavigationDestination("board/FXMLBoard.fxml", "ONLINLE BOARD");
 
     private static final Stack<NavigationDestination> destinations = new Stack<>();
 
@@ -80,8 +87,42 @@ public class TicTacToeNavigator {
         destinations.push(navigationDestination);
     }
 
+    public static void navigateLaterTo(Node node, NavigationDestination navigationDestination) {
+        navigateLaterTo((Stage) node.getScene().getWindow(), navigationDestination);
+    }
+
+    public static void navigateLaterTo(Stage stage, NavigationDestination navigationDestination) {
+        FXMLLoader loader = new FXMLLoader(xo.Xo.class.getResource(navigationDestination.getDestination()));
+        Platform.runLater(() -> {
+            try {
+                navigateTo(stage, FXMLLoader.load(xo.Xo.class.getResource(navigationDestination.getDestination())), navigationDestination.getTitle());
+                destinations.push(navigationDestination);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
+
+    public static void navigateLaterTo(Stage stage, Initializable controller, NavigationDestination navigationDestination) {
+        FXMLLoader loader = new FXMLLoader(xo.Xo.class.getResource(navigationDestination.getDestination()));
+        Platform.runLater(() -> {
+            try {
+                loader.setController(controller);
+                navigateTo(stage, (Parent) loader.load(), navigationDestination.getTitle());
+                destinations.push(navigationDestination);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
+
     public static void previous(Stage stage) throws IOException {
         navigateTo(stage, destinations.pop());
+    }
+
+    public static void previousLater(Stage stage) {
+        destinations.pop();
+        navigateLaterTo(stage, destinations.pop());
     }
 
     public static void previous(Event event) throws IOException {
@@ -89,7 +130,7 @@ public class TicTacToeNavigator {
         navigateTo(event, destinations.pop());
     }
 
-    public static void pop() {
+    private static void pop() {
         destinations.pop();
     }
 
