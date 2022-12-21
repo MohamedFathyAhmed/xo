2efddc25533ead179aeb1f85ce1607574eeb7cb3
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2022-12-21 07:50:06 +0200

    fix :sign in

--- src/xo/signin/FXMLSigninControler.java
+++ src/xo/signin/FXMLSigninControler.java
@@ -1,4 +1,3 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,32 +5,31 @@
  */
 package xo.signin;
 
-import data.CurrentGameData;
+import data.database.models.Player;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 import javafx.util.Pair;
-import xo.online.handlers.ParamterizeRequest;
-import xo.online.handlers.RequestHandler;
-import xo.online.handlers.Response;
-import xo.utlis.TicTacToeNavigator;
-
 
 /**
  * FXML Controller class
@@ -39,14 +37,13 @@ import xo.utlis.TicTacToeNavigator;
  * @author Marina
  */
 public class FXMLSigninControler implements Initializable {
-
+Thread thread;
     @FXML
     private ImageView levelsImagetwo;
-
     @FXML
     private TextField userNameTextField;
     @FXML
-    private PasswordField passwordPasswordField;
+    private TextField passwordTextField;
     @FXML
     private Label userNameLabel;
     @FXML
@@ -54,67 +51,99 @@ public class FXMLSigninControler implements Initializable {
     @FXML
     private Label signInLabel;
     @FXML
+    private Button backButton;
+    @FXML
     private Button loginButton;
     @FXML
     private Button signupButton;
     @FXML
-    private Button backButton;
-
-    private CurrentGameData currentGameData;
-    private RequestHandler requestHandler;
-
-    private Stage stage;
-
-    public FXMLSigninControler() {
-        currentGameData = CurrentGameData.getInstance();
-        requestHandler = RequestHandler.getInstance((Response response) -> {
-            if (response.isSuccess()) {
-                currentGameData.setOnlineName(userNameTextField.getText());
-                TicTacToeNavigator.navigateLaterTo(stage, TicTacToeNavigator.ONLINE_PLAYERS);
-            }
-        });
-    }
-
+    private Label passwordErrorLabel;
+    @FXML
+    private Label nameErrorLabel;
+    
+    Socket socket;
+    DataInputStream dis ;
+    PrintStream ps;
+    @FXML
+    private Button BtnIP;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }
+    }    
+     @FXML
+    private void backButtonClicked(ActionEvent event) {
 
-    @FXML
-    private void loginButtonClicked(ActionEvent event) {
-        if (stage == null) {
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        }
-        try {
-            requestHandler.create(RequestType.SIGNIN,
-                    new ParamterizeRequest(
-                            passwordPasswordField.getText(),
-                            userNameTextField.getText()));
-        } catch (IOException ex) {
-            //catch me
-            ex.printStackTrace();
-        }
     }
+     @FXML
+    private void loginButtonClicked(ActionEvent event) throws IOException {
+         boolean checkValidName;
+        boolean checkValidPassword;
 
-    @FXML
+        String userTxt = userNameTextField.getText();
+        String pasTxt = passwordTextField.getText();
+
+        if (userTxt.isEmpty()) {
+            checkValidName = false;
+            Platform.runLater(() -> nameErrorLabel.setText("Username is required"));
+        } else {
+            checkValidName = true;
+            nameErrorLabel.setText("");
+        }
+
+        if (pasTxt.isEmpty()) {
+            checkValidPassword = false;
+            Platform.runLater(() -> passwordErrorLabel.setText("Password is required"));
+        } else {
+            checkValidPassword = true;
+            passwordErrorLabel.setText("");
+        }
+        boolean checkValid = checkValidName && checkValidPassword;
+
+        if (checkValid) {
+
+            ps.println("signIn;;"+userTxt+";;"+pasTxt);
+            thread = new Thread(() -> {
+                try {
+                    String replyMsg;
+                    replyMsg = dis.readLine();
+                    System.out.println(replyMsg);
+                    String[] allReplyMsg = replyMsg.split(";;");
+                    if (allReplyMsg[0].equals("true")) {
+                        try {
+                            Player.setName(allReplyMsg[1]);
+                            Player.setScore(Integer.parseInt(allReplyMsg[2]));
+                        } catch (Exception e) {
+                            System.out.println(e);
+                        }
+                        // navigation
+                        //Platform.runLater(this::goToClientPage);
+                    } else {
+                        Platform.runLater(() -> {
+                           // alert
+                        });
+                        
+                        System.out.println(Player.getName());
+                    }
+                    thread.stop();
+                } catch (IOException ex) {
+                    Logger.getLogger(FXMLSigninControler.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+
+            thread.start();
+    }
+    
+    }
+    
+      @FXML
     private void signupButtonClicked(ActionEvent event) {
-        try {
-            TicTacToeNavigator.navigateTo(event, TicTacToeNavigator.SIGNUP);
-        } catch (IOException ex) {
-            //catch me
-        }
+
     }
 
     @FXML
-    private void backButtonClicked(ActionEvent event) throws IOException {
-        TicTacToeNavigator.previous(event);
-    }
-
-
- @FXML
     private void ipButtonClicked(ActionEvent event) {
           // Create the custom dialog.
     Dialog<Pair<String, String>> dialog = new Dialog<>();dialog.setTitle("Connect to server");dialog.setHeaderText("Please insert remoteIP and portNumber");
@@ -156,5 +185,6 @@ public class FXMLSigninControler implements Initializable {
         String portNum = connectToServer.getValue();
         // set ip and port
 });
-   
     }
+    
+}
